c17b0f5cc50b40bb7a01db836e603b6f2685d208
dariovargaos <dario.varga2012@gmail.com>
2023-06-02 20:20:34 +0200

    Add login and signup forms

--- finance-tracker/src/pages/login/Login.tsx
+++ finance-tracker/src/pages/login/Login.tsx
@@ -1,4 +1,56 @@
-import { Box } from "@chakra-ui/react";
+import { useState, FormEvent } from "react";
+import {
+  Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  FormHelperText,
+  Button,
+} from "@chakra-ui/react";
 export default function Login() {
-  return <Box>Login</Box>;
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = (e: FormEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit as any}
+      style={{
+        maxWidth: "360px",
+        margin: "60px auto",
+        padding: "20px",
+        boxShadow: "3px 3px 5px rgba(0,0,0, 0.05)",
+        border: "1px solid #ddd",
+      }}
+    >
+      <Heading mb="20px">Login</Heading>
+      <FormControl mb="20px">
+        <FormLabel>email:</FormLabel>
+        <Input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          fontSize="1em"
+          color="#777"
+        />
+      </FormControl>
+      <FormControl mb="20px">
+        <FormLabel>password:</FormLabel>
+        <Input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          fontSize="1em"
+          color="#777"
+        />
+      </FormControl>
+      <Button type="submit" color="#1f9752">
+        Login
+      </Button>
+    </form>
+  );
 }
--- finance-tracker/src/pages/signup/Signup.tsx
+++ finance-tracker/src/pages/signup/Signup.tsx
@@ -1,4 +1,67 @@
-import { Box } from "@chakra-ui/react";
+import { useState, FormEvent } from "react";
+import {
+  Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  FormHelperText,
+  Button,
+} from "@chakra-ui/react";
 export default function Signup() {
-  return <Box>Signup</Box>;
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [displayName, setDisplayName] = useState<string>("");
+
+  const handleSubmit = (e: FormEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    console.log(email, password, displayName);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit as any}
+      style={{
+        maxWidth: "360px",
+        margin: "60px auto",
+        padding: "20px",
+        boxShadow: "3px 3px 5px rgba(0,0,0, 0.05)",
+        border: "1px solid #ddd",
+      }}
+    >
+      <Heading mb="20px">Signup</Heading>
+      <FormControl mb="20px">
+        <FormLabel>email:</FormLabel>
+        <Input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          fontSize="1em"
+          color="#777"
+        />
+      </FormControl>
+      <FormControl mb="20px">
+        <FormLabel>password:</FormLabel>
+        <Input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          fontSize="1em"
+          color="#777"
+        />
+      </FormControl>
+      <FormControl mb="20px">
+        <FormLabel>display name:</FormLabel>
+        <Input
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+          fontSize="1em"
+          color="#777"
+        />
+      </FormControl>
+      <Button type="submit" color="#1f9752">
+        Login
+      </Button>
+    </form>
+  );
 }
