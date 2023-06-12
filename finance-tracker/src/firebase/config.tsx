8779c21f60594e87f4df3ddee55ba88393e115b2
dariovargaos <dario.varga2012@gmail.com>
2023-06-12 11:31:11 +0200

    Add file in gitignore to protect firebase config object

--- finance-tracker/src/firebase/config.tsx
+++ finance-tracker/src/firebase/config.tsx
@@ -3,12 +3,12 @@ import { getFirestore, Firestore, Timestamp } from "firebase/firestore";
 import { getAuth, Auth } from "firebase/auth";
 
 const firebaseConfig: object = {
-  apiKey: "AIzaSyDWqdlNIQu5bz5W4nw3XEod_ngug_7Syg4",
-  authDomain: "financetrackerts.firebaseapp.com",
-  projectId: "financetrackerts",
-  storageBucket: "financetrackerts.appspot.com",
-  messagingSenderId: "504122819622",
-  appId: "1:504122819622:web:5279c54255afd96ee9d2e7",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 //init firebase
